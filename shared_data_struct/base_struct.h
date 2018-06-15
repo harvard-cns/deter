5620413cb1ec0b39d4116b9240f22d76e9200d11
liyuliang001 <liyuliang001@gmail.com>
2018-06-14 21:15:53 -0400

    separate base_struct out

---
+++ shared_data_struct/base_struct.h
@@ -0,0 +1,58 @@
+#ifndef _SHARED_DATA_STRUCT__BASE_STRUCT_H
+#define _SHARED_DATA_STRUCT__BASE_STRUCT_H
+
+/* Different types of socket calls' ID starts with different highest 4 bits */
+#define DERAND_SOCK_ID_BASE 100
+
+#define DERAND_SOCKCALL_TYPE_SENDMSG 0
+struct derand_rec_sockcall_tcp_sendmsg{
+	u8 type;
+	int flags; // msg.msg_flags
+	size_t size;
+};
+#define DERAND_SOCKCALL_TYPE_RECVMSG 1
+struct derand_rec_sockcall_tcp_recvmsg{
+	u8 type;
+	int flags; // nonblock & flags
+	size_t size;
+};
+
+struct derand_rec_sockcall{
+	union{
+		u8 type;
+		struct derand_rec_sockcall_tcp_sendmsg sendmsg;
+		struct derand_rec_sockcall_tcp_recvmsg recvmsg;
+	};
+};
+
+#define EVENT_TYPE_TASKLET 1
+#define EVENT_TYPE_WRITE_TIMEOUT 2
+#define EVENT_TYPE_DELACK_TIMEOUT 3
+#define EVENT_TYPE_KEEPALIVE_TIMEOUT 4
+/* struct for each lock acquiring event */
+struct derand_event{
+	u32 seq;
+	u32 type; // 0: pkt; 1: tsq; 2~99: timeout types; 100 ~ inf: socket call IDs + 100
+};
+
+/* struct for a jiffies read with new value */
+union jiffies_rec{
+	unsigned long init_jiffies; // if this is the first jiffies read
+	struct {
+		s32 jiffies_delta;
+		u32 idx_delta;
+	};
+};
+
+/* struct for memory_allocated read with new value */
+union memory_allocated_rec{
+	long init_v;
+	struct {
+		u32 v_delta;
+		u32 idx_delta;
+	};
+};
+
+#define DERAND_EFFECT_BOOL_N_LOC 16
+
+#endif /* _SHARED_DATA_STRUCT__BASE_STRUCT_H */
--- shared_data_struct/derand_recorder.h
+++ shared_data_struct/derand_recorder.h
@@ -1,54 +1,12 @@
 #ifndef _SHARED_DATA_STRUCT__DERAND_RECORDER_H
 #define _SHARED_DATA_STRUCT__DERAND_RECORDER_H
 
+#include "base_struct.h"
 #include "tcp_sock_init_data.h"
-
-/* Different types of socket calls' ID starts with different highest 4 bits */
-#define DERAND_SOCK_ID_BASE 100
-
-#define DERAND_SOCKCALL_TYPE_SENDMSG 0
-struct derand_rec_sockcall_tcp_sendmsg{
-	u8 type;
-	int flags; // msg.msg_flags
-	size_t size;
-};
-#define DERAND_SOCKCALL_TYPE_RECVMSG 1
-struct derand_rec_sockcall_tcp_recvmsg{
-	u8 type;
-	int flags; // nonblock & flags
-	size_t size;
-};
-
-struct derand_rec_sockcall{
-	union{
-		u8 type;
-		struct derand_rec_sockcall_tcp_sendmsg sendmsg;
-		struct derand_rec_sockcall_tcp_recvmsg recvmsg;
-	};
-};
-
-#define EVENT_TYPE_TASKLET 1
-#define EVENT_TYPE_WRITE_TIMEOUT 2
-#define EVENT_TYPE_DELACK_TIMEOUT 3
-#define EVENT_TYPE_KEEPALIVE_TIMEOUT 4
-/* struct for each lock acquiring event */
-struct derand_event{
-	u32 seq;
-	u32 type; // 0: pkt; 1: tsq; 2~99: timeout types; 100 ~ inf: socket call IDs + 100
-};
 
 /************************************
  * jiffies
  ***********************************/
-/* struct for a jiffies read with new value */
-union jiffies_rec{
-	unsigned long init_jiffies; // if this is the first jiffies read
-	struct {
-		s32 jiffies_delta;
-		u32 idx_delta;
-	};
-};
-
 #define DERAND_JIFFIES_PER_SOCK 1024 
 struct jiffies_q{
 	unsigned long last_jiffies; // the last jiffies value
@@ -82,13 +40,6 @@ static inline void push_memory_pressure_q(struct memory_pressure_q *q, bool v){
  * memory_allocated
  ************************************/
 #define DERAND_MEMORY_ALLOCATED_PER_SOCK 1024
-union memory_allocated_rec{
-	long init_v;
-	struct {
-		u32 v_delta;
-		u32 idx_delta;
-	};
-};
 /* struct for memory_allocated */
 struct memory_allocated_q{
 	long last_v;
@@ -129,7 +80,6 @@ static inline void push_effect_bool_q(struct effect_bool_q *q, bool v){
 
 #define DERAND_EVENT_PER_SOCK 4096
 #define DERAND_SOCKCALL_PER_SOCK 4096
-#define DERAND_EFFECT_BOOL_N_LOC 16
 
 struct derand_recorder{
 	u32 sip, dip;
