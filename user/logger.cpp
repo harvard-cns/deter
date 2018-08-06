f6902b5d632e06107872d50d71b3da37f624186d
liyuliang001 <liyuliang001@gmail.com>
2018-08-06 17:18:11 -0400

    print out log at the end

--- user/logger.cpp
+++ user/logger.cpp
@@ -1,3 +1,5 @@
+#include <vector>
+#include <string>
 #include <thread>
 #include "kernel_typedef.hpp"
 #include "../shared_data_struct/logger.h"
@@ -6,6 +8,8 @@
 using namespace std;
 
 #define LOGGER_PROC_NAME "derand_logger"
+
+vector<string> logs;
 
 void test_thread(Logger *logger){
 	char buf[256];
@@ -29,15 +33,22 @@ int main(){
 
 	Logger *logger = (Logger*)kmem.buf;
 
-	thread th(test_thread, logger);
+	//thread th(test_thread, logger);
 	while (logger->running){
 		if (logger->h < (u32)logger->t.counter){
 			volatile uint8_t &ready = logger->buf[get_logger_idx(logger->h)][0];
-			while (!ready); // wait for ready
-			fprintf(stdout, "[%u %u] %s", logger->h, logger->t.counter, logger->buf[get_logger_idx(logger->h)] + 1);
+			for (uint32_t cnt = 1; !ready; cnt++){ // wait for ready
+				if ((cnt & 0x0fffffff) == 0)
+					printf("waiting for ready of entry[%u]\n", get_logger_idx(logger->h));
+			}
+			//fprintf(stdout, "[%u %u] %s", logger->h, logger->t.counter, logger->buf[get_logger_idx(logger->h)] + 1);
+			logs.push_back((char*)logger->buf[get_logger_idx(logger->h)] + 1);
 			ready = 0; // clear ready byte
 			logger->h++;
 		}
+	}
+	for (int i = 0; i < logs.size(); i++){
+		fprintf(stdout, "%s", logs[i].c_str());
 	}
 
 	kmem.unmap_mem();
