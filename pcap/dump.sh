244ba6df68ce2995cf32c1006939f0fe072d0b48
liyuliang001 <liyuliang001@gmail.com>
2018-08-08 15:46:48 -0400

    x

--- pcap/dump.sh
+++ pcap/dump.sh
@@ -1,5 +1,5 @@
 if [ $# -ne 1 ]; then
 	echo "usage: bash dump.sh <pcap_name>"
 else
-	sudo tcpdump -s 96 -i ens8 -w $1 -B 10000000 port 60000
+	sudo tcpdump -s 96 -i ens8 -w $1 -B 10000000 portrange 60000-60003 or port 50010
 fi
