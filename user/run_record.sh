379a9b193464a597ff2898fc2d303cbd83da0772
liyuliang001 <liyuliang001@gmail.com>
2018-08-08 15:36:17 -0400

    add dstip to run_record.sh

--- user/run_record.sh
+++ user/run_record.sh
@@ -1,10 +1,20 @@
-cd ../pcap
-bash dump.sh runtime.pcap &
+if [ $# -ge 2 ]; then
+	echo "usage: bash run_record.sh [<dstip(hex)>]"
+	echo "       <dstip(hex)>: specific the connection to record. Should be in hex"
+else
+	if [ $# -eq 1 ]; then
+		dstip=$1
+	else
+		dstip=0
+	fi
+	cd ../pcap
+	bash dump.sh runtime.pcap &
 
-sleep 1
+	sleep 1
 
-cd ../kmod
-sudo insmod derand_recorder.ko
+	cd ../kmod
+	sudo insmod derand_recorder.ko dstip=$dstip
 
-cd ../user
-sudo ./recorder
+	cd ../user
+	sudo ./recorder
+fi
