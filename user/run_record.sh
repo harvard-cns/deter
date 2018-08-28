3f576f50ac1dec31a18c2539cf6dec7338884414
liyuliang001 <liyuliang001@gmail.com>
2018-08-28 15:22:42 -0400

    enhance run_record.sh

--- user/run_record.sh
+++ user/run_record.sh
@@ -1,23 +1,44 @@
 source ip_util.sh
 
-if [ $# -ge 2 ]; then
-	echo "usage: bash run_record.sh [<dstip>]"
-	echo "       <dstip>: specific the connection to record."
-else
-	if [ $# -eq 1 ]; then
-		dstip=$1
-	else
-		dstip=0.0.0.0
-	fi
-	dstip_int=$(ip_to_int $dstip)
+dstip=0.0.0.0
+do_tcpdump=0
+while [[ $# -gt 0 ]]
+do
+	key=$1
+	case $key in 
+	-h|--help)
+		echo "usage: bash run_record.sh [OPTION]..."
+		echo "options:"
+		echo "-d, --dstip             specify the dstip of the connection to record"
+		echo "-p, --tcpdump           do tcpdump"
+		shift
+		exit 0
+	;;
+	-d|--dstip)
+		dstip=$2
+		shift
+		shift
+	;;
+	-p|--tcpdump)
+		do_tcpdump=1
+		shift
+	;;
+	*)
+		echo "unknown argument:" $key
+		shift
+	esac
+done
+
+dstip_int=$(ip_to_int $dstip)
+
+if [ $do_tcpdump -eq 1 ]; then
 	cd ../pcap
 	bash dump.sh runtime.pcap "(host $dstip)" &
-
-	sleep 1
-
-	cd ../kmod
-	sudo insmod derand_recorder.ko dstip=$dstip_int
-
-	cd ../user
-	sudo ./recorder
+	sleep 1.5
 fi
+
+cd ../kmod
+sudo insmod derand_recorder.ko dstip=$dstip_int
+
+cd ../user
+sudo ./recorder
