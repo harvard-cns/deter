4507ed30e0f1bf3b7a8d683cef02efb2564633e5
Ubuntu <liyuliang001@gmail.com>
2018-09-15 17:48:07 +0000

    optmize accounting.y

--- user/accounting.py
+++ user/accounting.py
@@ -2,7 +2,7 @@ import glob
 import subprocess
 import re
 if __name__ == "__main__":
-	records = glob.glob("records_spark/0a00000*")
+	records = glob.glob("log/*")
 	total_bytes = 0
 	total_mstamp = 0
 	total_evts = 0
@@ -25,78 +25,82 @@ if __name__ == "__main__":
 		if srcip == dstip:
 			continue
 		print srcip, dstip
-		cmd = "./reader '%s' get_meta | grep 'broken' | cut -d' ' -f2"%rec
+
+		# put result in a temp file
+		cmd = "./reader '%s' > tmp_result.txt"%rec
+		subprocess.call(cmd, shell=True)
+
+		cmd = "cat tmp_result.txt | grep 'broken' | cut -d' ' -f2"
 		out = subprocess.check_output(cmd, shell=True).strip()
 		if out != '0':
 			print "broken %s, skip"%out
 			#continue
 
-		cmd = "./reader '%s' get_meta | grep 'compressed total' | cut -d' ' -f3"%rec
+		cmd = "cat tmp_result.txt | grep 'compressed total' | cut -d' ' -f3"
 		out = subprocess.check_output(cmd, shell=True).strip()
 		total_bytes += int(out)
 		print "total:", out
 
-		cmd = "./reader '%s' get_meta | grep '^mstamp' | cut -d' ' -f7"%rec
+		cmd = "cat tmp_result.txt | grep '^mstamp' | cut -d' ' -f7"
 		out = subprocess.check_output(cmd, shell=True).strip()
 		total_mstamp += int(out)
 		print "mstamp:", out
 
-		cmd = "./reader '%s' get_meta | grep '^evts:' | cut -d' ' -f2"%rec
+		cmd = "cat tmp_result.txt | grep '^evts:' | cut -d' ' -f2"
 		out = subprocess.check_output(cmd, shell=True).strip()
 		total_evts += int(out)
 		print "evts:", out
 
-		cmd = "./reader '%s' get_meta | grep '^\tevts: dynamic:' | cut -d' ' -f3"%rec
+		cmd = "cat tmp_result.txt | grep '^\tevts: dynamic:' | cut -d' ' -f3"
 		out = subprocess.check_output(cmd, shell=True).strip()
 		total_evts_dynamic += int(out)
 		print "evts, dynamic:", out
 
-		cmd = "./reader '%s' get_meta | grep '^\tevts: huffman_prefix:' | cut -d' ' -f3"%rec
+		cmd = "cat tmp_result.txt | grep '^\tevts: huffman_prefix:' | cut -d' ' -f3"
 		out = subprocess.check_output(cmd, shell=True).strip()
 		total_evts_huffman_prefix += int(out)
 		print "evts, huffman_prefix:", out
 
-		cmd = "./reader '%s' get_meta | grep '^\tevts: huffman_encoding:' | cut -d' ' -f3"%rec
+		cmd = "cat tmp_result.txt | grep '^\tevts: huffman_encoding:' | cut -d' ' -f3"
 		out = subprocess.check_output(cmd, shell=True).strip()
 		total_evts_huffman_encoding += int(out)
 		print "evts, huffman_encoding:", out
 
-		cmd = "./reader '%s' get_meta | grep '^sockcalls:' | cut -d' ' -f2"%rec
+		cmd = "cat tmp_result.txt | grep '^sockcalls:' | cut -d' ' -f2"
 		out = subprocess.check_output(cmd, shell=True).strip()
 		total_sockcall += int(out)
 		print "sockcalls:", out
 
-		cmd = "./reader '%s' get_meta | grep '^jiffies:' | cut -d' ' -f2"%rec
+		cmd = "cat tmp_result.txt | grep '^jiffies:' | cut -d' ' -f2"
 		out = subprocess.check_output(cmd, shell=True).strip()
 		total_jiffies += int(out)
 
-		#cmd = "./reader '%s' | grep 'tcp_transmit_skb\[0' | wc -l"%rec
-		cmd = "./reader '%s' get_meta | grep 'packets received' | cut -d' ' -f3"%rec
+		cmd = "cat tmp_result.txt | grep 'packets received' | cut -d' ' -f3"
 		out = subprocess.check_output(cmd, shell=True).strip()
 		total_tcpdump += int(out)
-		print "packets:", out
+		print "packets:", out, total_tcpdump
 
-		cmd = "./reader '%s' get_meta | grep 'tx_stamp size, huffman_prefix' | cut -d' ' -f4"%rec
+		cmd = "cat tmp_result.txt | grep 'tx_stamp size, huffman_prefix' | cut -d' ' -f4"
 		out = subprocess.check_output(cmd, shell=True).strip()
 		total_tx_stamp += int(out)
 		print "tx_stamp, huffman_prefix:", out
 
-		cmd = "./reader '%s' get_meta | grep 'tx_stamp size, sample, dynamic' | cut -d' ' -f8"%rec
+		cmd = "cat tmp_result.txt | grep 'tx_stamp size, sample, dynamic' | cut -d' ' -f8"
 		out = subprocess.check_output(cmd, shell=True).strip()
 		total_tx_stamp_sample_dynamic += int(out)
 		print "tx_stamp, sample, dynamic:", out
 
-		cmd = "./reader '%s' get_meta | grep 'tx_stamp size, sample, huffman_prefix' | cut -d' ' -f8"%rec
+		cmd = "cat tmp_result.txt | grep 'tx_stamp size, sample, huffman_prefix' | cut -d' ' -f8"
 		out = subprocess.check_output(cmd, shell=True).strip()
 		total_tx_stamp_sample_huffman_prefix += int(out)
 		print "tx_stamp, sample, huffman_prefix:", out
 
-		cmd = "./reader '%s' | grep 'drops' | cut -d' ' -f1"%rec
+		cmd = "cat tmp_result.txt | grep 'drops' | cut -d' ' -f1"
 		out = subprocess.check_output(cmd, shell=True).strip()
 		total_loss += int(out)
 		print "loss:", out
 
-		cmd = "./reader '%s' | grep 'fin_seq' | cut -d' ' -f2"%rec
+		cmd = "cat tmp_result.txt | grep 'fin_seq' | cut -d' ' -f2"
 		out = subprocess.check_output(cmd, shell=True).strip()
 		total_transfer += int(out)
 		print "transfer (Bytes):", out
